29eccb209826970003869535d3d66b624c985640
awesomearjun <64511698+awesomearjun@users.noreply.github.com>
2022-11-30 21:29:43 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+
+time.sleep(2)
+
+pyautogui.write('yt.be')
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.write('/Arj404')
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.press('tab')
+
+pyautogui.press('enter')
